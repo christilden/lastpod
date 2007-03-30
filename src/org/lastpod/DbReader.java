7ef565a5f99a09d9d3601003c213ef9af168ce94
chris <chris@1be3d916-834f-4e11-84c0-b560dc06693e>
2007-03-30 07:19:25 +0000

    r7526@flan:  chris | 2007-03-03 15:53:48 -0800  added GPL terms to all sorce files
    git-svn-id: http://svn.lastpod.org/trunk@27 1be3d916-834f-4e11-84c0-b560dc06693e

--- src/org/lastpod/DbReader.java
+++ src/org/lastpod/DbReader.java
@@ -1,3 +1,22 @@
+/* 
+ * LastPod is an application used to publish one's iPod play counts to Last.fm.
+ * Copyright (C) 2007  muti, Chris Tilden
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * package org.lastpod;
+ */
 package org.lastpod;
 
 import java.io.BufferedInputStream;
@@ -13,7 +32,7 @@ import java.util.Collections;
 
 /**
  * @author muti
- *
+ * @version $Id$
  */
 public class DbReader {
     private String itunesfile;
