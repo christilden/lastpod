98e29ea37375271142176f0257fc17a569d6bb92
chris <chris@1be3d916-834f-4e11-84c0-b560dc06693e>
2007-04-23 06:53:10 +0000

    r7627@flan:  chris | 2007-04-22 23:52:47 -0700  beefing up the preferences editor using code from audiopodGoesIscrobbler  Completes Trac ticket #3 (automatically find iTunes DB file) and    #4 (launch iTunes.exe on exit)
    git-svn-id: http://svn.lastpod.org/trunk@39 1be3d916-834f-4e11-84c0-b560dc06693e

--- src/org/lastpod/PreferencesEditor.java
+++ src/org/lastpod/PreferencesEditor.java
@@ -19,24 +19,34 @@
  */
 package org.lastpod;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import java.io.File;
 
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.prefs.Preferences;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SpringLayout;
+import javax.swing.border.TitledBorder;
 
 /**
  * @author Muti
@@ -49,6 +59,13 @@ public class PreferencesEditor {
     private JTextField userfield;
     private JPasswordField passfield;
     private JTextField dbfield;
+    private JTextField iTunesfield;
+    private JCheckBox iTCheck;
+    private JLabel iTunesStatus;
+    private JButton browsebuttoniTunes;
+
+    public PreferencesEditor() {
+    }
 
     public void buildUI() {
         this.frame = new JFrame("Preferences...");
@@ -59,38 +76,211 @@ public class PreferencesEditor {
         this.matchPreferences(); //gets preferences from LastPod and updates UI
 
         this.frame.pack();
+
+        int x =
+            (int) ((Toolkit.getDefaultToolkit().getScreenSize().width / 2) - (frame.getWidth() / 2));
+        int y =
+            (int) ((Toolkit.getDefaultToolkit().getScreenSize().height / 2)
+            - (frame.getHeight() / 2));
+        frame.setLocation(x, y);
+        this.frame.setResizable(false);
         this.frame.setVisible(true);
     }
 
     private void addElements() {
-        //TODO add mnemonics to buttons
-        JPanel p = new JPanel(new SpringLayout());
+        JPanel p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
         p.setOpaque(true);
         this.frame.setContentPane(p);
 
-        JLabel userlabel = new JLabel("AudioScrobbler Username:");
-        p.add(userlabel);
-        this.userfield = new JTextField();
-        p.add(this.userfield);
-        p.add(new JLabel());
+        //Username and password panel
+        JPanel p2 = new JPanel();
+        p2.setLayout(new SpringLayout());
 
-        JLabel passlabel = new JLabel("AudioScrobbler Password:");
-        p.add(passlabel);
+        TitledBorder b2 = BorderFactory.createTitledBorder("AudioScrobbler/Last.fm:");
+        p2.setBorder(b2);
+        p2.setToolTipText(
+            "<html>This entry is used if you want to <b>Submit Tracks</b> to AudioScrobbler/Last.fm");
+
+        JLabel userlabel = new JLabel("Username:");
+        p2.add(userlabel);
+        this.userfield = new JTextField();
+        p2.add(this.userfield);
+
+        JLabel passlabel = new JLabel("Password:");
+        p2.add(passlabel);
         this.passfield = new JPasswordField();
-        p.add(this.passfield);
-        p.add(new JLabel());
+        p2.add(this.passfield);
+
+        SpringUtilities.makeCompactGrid(p2, 2, 2, 5, 4, 3, 4);
+        p.add(p2);
+
+        //iPod Panel
+        JPanel p1 = new JPanel();
+        p1.setLayout(new SpringLayout());
+
+        TitledBorder b1 = BorderFactory.createTitledBorder("iPod:");
+        p1.setBorder(b1);
+        p1.setToolTipText(
+            "<html>Path of the iTunesDB:<br>Located on your iPod, e.g. <b>M</b>:\\iPod_Control\\iTunes,<br>where M is the drive letter of your iPod.");
 
         JLabel dblabel = new JLabel("Location of iTunesDB:");
-        p.add(dblabel);
+        p1.add(dblabel);
         this.dbfield = new JTextField();
-        p.add(this.dbfield);
+        this.dbfield.setPreferredSize(new Dimension(200, 20));
+        p1.add(this.dbfield);
 
-        JButton browsebutton = new JButton("Browse..");
-        browsebutton.setMnemonic(KeyEvent.VK_R);
-        browsebutton.addActionListener(new BrowseButtonListener());
-        p.add(browsebutton);
+        JButton BrowseButtoniPod = new JButton("Browse..");
+        BrowseButtoniPod.setMnemonic(KeyEvent.VK_B);
+        BrowseButtoniPod.addActionListener(new BrowseButtonListeneriPod());
+        p1.add(BrowseButtoniPod);
+        SpringUtilities.makeCompactGrid(p1, 1, 3, 5, 4, 3, 4);
+        p.add(p1);
 
-        p.add(new JLabel());
+        //iTunes Panel
+        JPanel p4 = new JPanel();
+        p4.setLayout(new SpringLayout());
+
+        TitledBorder b4 = BorderFactory.createTitledBorder("iTunes:");
+        p4.setBorder(b4);
+        p4.setToolTipText("Enable if you want to use audioPod with iTunes");
+
+        JPanel p41 = new JPanel();
+        iTCheck = new JCheckBox();
+        iTCheck.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if (iTCheck.isSelected()) {
+                        iTunesStatus.setText("Enabled");
+                    } else {
+                        iTunesStatus.setText("Disabled");
+                    }
+                }
+            });
+        p41.add(iTCheck);
+        iTunesStatus = new JLabel();
+        iTunesStatus.addMouseListener(new MouseListener() {
+                public void mouseClicked(MouseEvent e) {
+                    if (iTunesStatus.getText().equals("Disabled")) {
+                        iTCheck.setSelected(true);
+                        iTunesStatus.setText("Enabled");
+                        iTunesfield.setEditable(true);
+                        browsebuttoniTunes.setEnabled(true);
+                    } else {
+                        iTCheck.setSelected(false);
+                        iTunesStatus.setText("Disabled");
+                        iTunesfield.setEditable(false);
+                        browsebuttoniTunes.setEnabled(false);
+                    }
+                }
+
+                public void mouseEntered(MouseEvent e) {
+                }
+
+                public void mouseExited(MouseEvent e) {
+                }
+
+                public void mouseReleased(MouseEvent e) {
+                }
+
+                public void mousePressed(MouseEvent e) {
+                }
+            });
+        p41.add(iTunesStatus);
+
+        p4.add(p41);
+        p4.add(new JLabel());
+        p4.add(new JLabel());
+
+        JLabel exelabel = new JLabel("Location of iTunes.exe:");
+        p4.add(exelabel);
+        this.iTunesfield = new JTextField();
+        this.iTunesfield.setPreferredSize(new Dimension(200, 20));
+        p4.add(this.iTunesfield);
+
+        browsebuttoniTunes = new JButton("Browse..");
+        browsebuttoniTunes.setMnemonic(KeyEvent.VK_R);
+        browsebuttoniTunes.addActionListener(new BrowseButtonListenerITunes());
+        p4.add(browsebuttoniTunes);
+        SpringUtilities.makeCompactGrid(p4, 2, 3, 5, 4, 3, 4);
+        p.add(p4);
+
+        //Options Panel
+        JPanel p3 = new JPanel();
+        p3.setLayout(new SpringLayout());
+
+        TitledBorder b3 = BorderFactory.createTitledBorder("Options:");
+        p3.setBorder(b3);
+
+        String toolTip =
+            "<html>If a URL is entered the play information will be"
+            + " submitted to both Last.fm and the given URL.  This allows one"
+            + " to perform a backup of the Last.fm data.";
+        p3.setToolTipText(toolTip);
+
+        JLabel j3 = new JLabel("Backup URL: ");
+        p3.add(j3);
+        p3.add(new JTextField());
+
+        SpringUtilities.makeCompactGrid(p3, 1, 2, 5, 2, 3, 4);
+        p.add(p3);
+
+        JPanel p5 = new JPanel();
+
+        JButton findPathsButton = new JButton("Find Paths");
+        findPathsButton.setMnemonic(KeyEvent.VK_F);
+        findPathsButton.setToolTipText("Will try to find the paths for iTunesDB and iTunes.exe");
+        findPathsButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    /* Scan for iTunesDB. */
+                    File itdb = new File(dbfield.getText());
+                    File itdbFile = new File(dbfield.getText() + "\\Play Counts");
+                    boolean dbFound = false;
+
+                    if (itdbFile.isFile() || itdb.isFile()
+                            || itdb.getAbsolutePath().endsWith(":\\iPod_Control\\iTunes")) {
+                        iTunesfield.setText(itdb.getAbsolutePath());
+                        dbFound = true;
+                    } else {
+                        for (int i = (int) 'B'; i <= (int) 'Z'; i++) {
+                            itdb = new File((char) i + ":\\iPod_Control\\iTunes\\iTunesDB");
+
+                            if (itdb.isFile()) {
+                                dbfield.setText(itdb.getParent());
+                                dbFound = true;
+
+                                break;
+                            }
+                        }
+
+                        if (!dbFound) {
+                            JOptionPane.showMessageDialog(new JFrame(),
+                                "iTunesDB location not found. Are you sure your iPod is plugged in?");
+                            dbfield.setText("<iPod iTunes Database Location>");
+                        }
+                    }
+
+                    /* Scan for iTunes. */
+                    File itProgram = new File(iTunesfield.getText());
+                    File itProgramFile = new File(iTunesfield.getText() + "\\iTunes.exe");
+
+                    if (itProgram.isFile()) {
+                        iTunesfield.setText(itProgram.getParent());
+                    } else if (itProgramFile.isFile()) {
+                        iTunesfield.setText(itProgramFile.getParent());
+                    } else {
+                        for (int i = (int) 'B'; i <= (int) 'Z'; i++) {
+                            itProgram = new File((char) i + ":\\Program Files\\iTunes\\iTunes.exe");
+
+                            if (itProgram.isFile()) {
+                                iTunesfield.setText(itProgram.getParent());
+
+                                break;
+                            }
+                        }
+                    }
+                }
+            });
+        p5.add(findPathsButton);
 
         JButton okbutton = new JButton("OK");
         okbutton.setMnemonic(KeyEvent.VK_O);
@@ -100,7 +290,7 @@ public class PreferencesEditor {
                     frame.dispose();
                 }
             });
-        p.add(okbutton);
+        p5.add(okbutton);
 
         JButton cancelbutton = new JButton("Cancel");
         cancelbutton.setMnemonic(KeyEvent.VK_C);
@@ -109,14 +299,27 @@ public class PreferencesEditor {
                     frame.dispose();
                 }
             });
-        p.add(cancelbutton);
+        p5.add(cancelbutton);
 
-        SpringUtilities.makeCompactGrid(p, 4, 3, 6, 6, 6, 6);
+        p.add(p5);
     }
 
     private void matchPreferences() {
         this.userfield.setText(fPrefs.get("Username", "<Username>"));
         this.dbfield.setText(fPrefs.get("iTunes Path", "<iPod iTunes Database Location>"));
+        this.iTunesfield.setText(fPrefs.get("iT Path", "   < Enter iTunes path >   "));
+
+        if (fPrefs.get("iTunes Status", "Enabled").equals("Enabled")) {
+            this.iTunesStatus.setText("Enabled");
+            this.iTCheck.setSelected(true);
+            this.iTunesfield.setEditable(true);
+            this.browsebuttoniTunes.setEnabled(true);
+        } else {
+            this.iTunesStatus.setText("Disabled");
+            this.iTCheck.setSelected(false);
+            this.iTunesfield.setEditable(false);
+            this.browsebuttoniTunes.setEnabled(false);
+        }
     }
 
     private void savePreferences() {
@@ -130,6 +333,8 @@ public class PreferencesEditor {
         }
 
         fPrefs.put("iTunes Path", this.dbfield.getText());
+        fPrefs.put("iT Path", this.iTunesfield.getText());
+        fPrefs.put("iTunes Status", this.iTunesStatus.getText());
 
         try {
             fPrefs.flush();
@@ -138,20 +343,52 @@ public class PreferencesEditor {
             logger.log(Level.WARNING, "Unable to save preferences: " + e.toString());
         }
 
+        LastPod.recentplayed = new ArrayList();
         LastPod.ParsePlayCounts();
+        LastPod.UI.newTrackListAvailable();
     }
 
-    private class BrowseButtonListener implements ActionListener {
+    private class BrowseButtonListeneriPod implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             JFileChooser fc = new JFileChooser();
             fc.setFileHidingEnabled(false);
             fc.setFileFilter(new ItunesdbFilter());
+
+            File d = new File(dbfield.getText());
+
+            if (d.isDirectory()) {
+                fc.setCurrentDirectory(d);
+            }
 
             int returnVal = fc.showOpenDialog(frame);
 
             if (returnVal == JFileChooser.APPROVE_OPTION) {
                 File f = fc.getSelectedFile();
                 dbfield.setText(f.getParent());
+            }
+        }
+    }
+
+    private class BrowseButtonListenerITunes implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JFileChooser fc = new JFileChooser();
+            fc.setFileHidingEnabled(false);
+
+            File d = new File(iTunesfield.getText());
+
+            if (d.isDirectory()) {
+                fc.setCurrentDirectory(d);
+            } else if (d.isFile()) {
+                fc.setCurrentDirectory(d.getParentFile());
+            }
+
+            fc.setFileFilter(new ExeFileFilter());
+
+            int returnVal = fc.showOpenDialog(frame);
+
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                File f = fc.getSelectedFile();
+                iTunesfield.setText(f.getParent());
             }
         }
     }
--- src/org/lastpod/UI.java
+++ src/org/lastpod/UI.java
@@ -26,6 +26,12 @@ import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import java.io.IOException;
+
+import java.util.prefs.Preferences;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -52,7 +58,31 @@ public class UI {
     public void buildUI() {
         JFrame.setDefaultLookAndFeelDecorated(true);
         frame = new JFrame("LastPod");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        /* If enabled launch iTunes after exiting the application. */
+        frame.addWindowListener(new WindowAdapter() {
+                public void windowClosing(WindowEvent windowEvent) {
+                    Preferences fPrefs = Preferences.userRoot().node("ws/afterglo/audioPod");
+                    String iTunesStatus = fPrefs.get("iTunes Status", "Disabled");
+
+                    if (iTunesStatus.equals("Enabled")) {
+                        String iTunesPath = fPrefs.get("iT Path", "default");
+
+                        if (!iTunesPath.endsWith("iTunes.exe")) {
+                            iTunesPath += "\\iTunes.exe";
+                        }
+
+                        try {
+                            Runtime rt = Runtime.getRuntime();
+                            rt.exec(iTunesPath);
+                        } catch (IOException e) {
+                            System.out.println(iTunesPath + " not found!  Cannot launch iTunes.");
+                        }
+                    }
+
+                    System.exit(0);
+                }
+            });
 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         Dimension frameSize = new Dimension(screenSize);
